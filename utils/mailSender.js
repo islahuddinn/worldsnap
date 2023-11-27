1f5f4315a2b621735d9c7da7fb8786c9c5ee0c52
islahuddin <m.chsallu87@gmail.com>
2023-11-27 17:39:41 +0500

    Impleminting forget password and send email functionalities

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,30 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (options) => {
-//   // 1) create a transporter
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
-
-//   // 2) Define the email options
-//   const mailOptions = {
-//     from: "islahuddin <islahuddin87@gmail.com>",
-//     to: options.email,
-//     subject: options.subject,
-//     text: options.message,
-//   };
-//   // 3) Actually send the email
-
-//   await transporter.sendMail(mailOptions);
-// };
-
-// module.exports = sendEmail;
-
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
@@ -38,8 +11,9 @@ const sendEmail = async (options) => {
         pass: process.env.EMAIL_PASSWORD,
       },
       tls: {
-        rejectUnauthorized: false, // Accept self-signed certificates
+        rejectUnauthorized: false,
       },
+      timeout: 10000,
     });
 
     // 2) Define the email options
@@ -56,7 +30,6 @@ const sendEmail = async (options) => {
     console.log("Email sent successfully!");
   } catch (error) {
     console.error("Error sending email:", error);
-    // Handle the error accordingly, you might want to throw it or return a specific value
   }
 };
 
